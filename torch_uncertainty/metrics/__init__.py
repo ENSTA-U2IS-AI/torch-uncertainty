0bc26f7711b95ab072b9157a9f60410db1d1e4c2
alafage <adrienlafage@outlook.com>
2023-02-02 23:51:23 +0100

    Adds ClassificationEnsemble routine
    + metrics (Disagreement, JensenShannonDivergence, MutualInformation)

--- torch_uncertainty/metrics/__init__.py
+++ torch_uncertainty/metrics/__init__.py
@@ -2,3 +2,6 @@
 from .entropy import Entropy
 from .fpr95 import FPR95Metric
 from .nll import NegativeLogLikelihood
+from .disagreement import DisagreementMetric
+from .jensen_shannon_divergence import JensenShannonDivergence
+from .mutual_information import MutualInformation
