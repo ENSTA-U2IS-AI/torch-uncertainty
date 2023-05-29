662366c74d20a8c11cb7f1585470371f8439bdb2
alafage <adrienlafage@outlook.com>
2023-05-29 18:13:40 +0200

    Update tests accordingly to changes :hammer:

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,67 +1,38 @@
 # fmt: off
-from argparse import ArgumentParser
 from pathlib import Path
 
-import pytorch_lightning as pl
 import torch.nn as nn
 from cli_test_helpers import ArgvContext
 
-from torch_uncertainty import cli_main, main
-from torch_uncertainty.baselines.standard import ResNet
+from torch_uncertainty import cls_main, init_args
+from torch_uncertainty.baselines import ResNet
 from torch_uncertainty.datamodules import CIFAR10DataModule
 from torch_uncertainty.optimization_procedures import optim_cifar10_resnet18
-
-from ._dummies import Dummy, DummyDataModule
 
 
 # fmt: on
 class TestCLI:
     """Testing the CLI function."""
 
-    def test_main_summary(self):
-        root = Path(__file__).parent.absolute().parents[0]
-
-        parser = ArgumentParser("torch-uncertainty")
-        parser.add_argument("--seed", type=int, default=None)
-        parser.add_argument("--test", type=int, default=None)
-        parser.add_argument("--summary", dest="summary", action="store_true")
-        parser.add_argument(
-            "--log_graph", dest="log_graph", action="store_true"
-        )
-        parser.add_argument(
-            "--channels_last",
-            action="store_true",
-            help="Use channels last memory format",
-        )
-
-        datamodule = CIFAR10DataModule
-        network = ResNet
-        parser = pl.Trainer.add_argparse_args(parser)
-        parser = datamodule.add_argparse_args(parser)
-        parser = network.add_model_specific_args(parser)
-
-        # Simulate that summary is True & the only argument
-        args = parser.parse_args(["--no-imagenet_structure"])
-        args.summary = True
-
-        main(
-            network,
-            datamodule,
-            nn.CrossEntropyLoss,
-            optim_cifar10_resnet18,
-            root,
-            "std",
-            args,
-        )
-
-    def test_cli_main(self):
+    def test_cls_main_summary(self):
         root = Path(__file__).parent.absolute().parents[0]
         with ArgvContext(""):
-            cli_main(
-                Dummy,
-                DummyDataModule,
-                nn.CrossEntropyLoss,
-                optim_cifar10_resnet18,
-                root,
-                "dummy",
+            args = init_args(ResNet, CIFAR10DataModule)
+
+            # datamodule
+            args.root = str(root / "data")
+            dm = CIFAR10DataModule(**vars(args))
+
+            # Simulate that summary is True & the only argument
+            args.summary = True
+
+            model = ResNet(
+                num_classes=dm.num_classes,
+                in_channels=dm.num_channels,
+                loss=nn.CrossEntropyLoss,
+                optimization_procedure=optim_cifar10_resnet18,
+                imagenet_structure=False,
+                **vars(args),
             )
+
+            cls_main(model, dm, root, "std", args)
