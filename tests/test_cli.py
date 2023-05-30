2147bcad952d32105428075d9c08828b1a61251e
Olivier <olivier.ar.laurent@gmail.com>
2023-05-30 23:14:56 +0200

    Second CLI test with different arguments :heavy_check_mark:

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -36,3 +36,26 @@ class TestCLI:
             )
 
             cls_main(model, dm, root, "std", args)
+
+    def test_cls_main_other_arguments(self):
+        root = Path(__file__).parent.absolute().parents[0]
+        with ArgvContext("--seed 42 --max_epochs 1 --channels_last"):
+            args = init_args(ResNet, CIFAR10DataModule)
+
+            # datamodule
+            args.root = root / "data"
+            dm = CIFAR10DataModule(**vars(args))
+
+            # Simulate that summary is True & the only argument
+            args.summary = True
+
+            model = ResNet(
+                num_classes=dm.num_classes,
+                in_channels=dm.num_channels,
+                loss=nn.CrossEntropyLoss,
+                optimization_procedure=optim_cifar10_resnet18,
+                imagenet_structure=False,
+                **vars(args),
+            )
+
+            cls_main(model, dm, root, "std", args)
