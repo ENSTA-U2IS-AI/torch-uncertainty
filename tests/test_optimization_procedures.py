e9ae77719589d6a676207e7ecc97fadc8fdaf931
Olivier <olivier.ar.laurent@gmail.com>
2023-05-30 23:09:02 +0200

    Use get_procedure in opt. proc. tests :heavy_check_mark:

--- tests/test_optimization_procedures.py
+++ tests/test_optimization_procedures.py
@@ -1,36 +1,42 @@
 # flake8: noqa
 # fmt: off
 from torch_uncertainty.models.resnet import resnet18, resnet50
-
-# from torch_uncertainty.models.wideresnet import resnet18, resnet50
+from torch_uncertainty.models.wideresnet import wideresnet28x10
 from torch_uncertainty.optimization_procedures import *
+from torch_uncertainty.optimization_procedures import get_procedure
 
 
 # fmt: on
 class TestOptProcedures:
     def test_optim_cifar10_resnet18(self):
+        procedure = get_procedure("resnet18", "cifar10", "standard")
         model = resnet18(in_channels=3, num_classes=10)
-        optim_cifar10_resnet18(model)
+        procedure(model)
 
     def test_optim_cifar10_resnet50(self):
+        procedure = get_procedure("resnet50", "cifar10", "packed")
         model = resnet50(in_channels=3, num_classes=10)
-        optim_cifar10_resnet50(model)
+        procedure(model)
 
-    # def test_optim_cifar10_wideresnet(self):
-    #     model = resnet50()
-    #     optim_cifar10_wideresnet(model)
+    def test_optim_cifar10_wideresnet(self):
+        procedure = get_procedure("wideresnet28x10", "cifar100", "batched")
+        model = wideresnet28x10(in_channels=3, num_classes=10)
+        procedure(model)
 
     def test_optim_cifar100_resnet18(self):
+        procedure = get_procedure("resnet50", "cifar100", "masked")
         model = resnet18(in_channels=3, num_classes=100)
-        optim_cifar100_resnet18(model)
+        procedure(model)
 
     def test_optim_cifar100_resnet50(self):
+        procedure = get_procedure("resnet50", "cifar100")
         model = resnet50(in_channels=3, num_classes=100)
-        optim_cifar100_resnet50(model)
+        procedure(model)
 
-    # def test_optim_cifar100_wideresnet(self):
-    #     model = resnet50()
-    #     optim_cifar10_wideresnet(model)
+    def test_optim_cifar100_wideresnet(self):
+        procedure = get_procedure("wideresnet28x10", "cifar100")
+        model = wideresnet28x10(in_channels=3, num_classes=100)
+        procedure(model)
 
     def test_optim_imagenet_resnet50(self):
         model = resnet50(in_channels=3, num_classes=1000)
