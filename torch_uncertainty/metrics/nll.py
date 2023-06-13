44e552a9ea3dabace633dd370e0f14715f052469
Olivier <olivier.ar.laurent@gmail.com>
2023-06-13 20:42:22 +0200

    Remove GNNL duplicate code :fire:

--- torch_uncertainty/metrics/nll.py
+++ torch_uncertainty/metrics/nll.py
@@ -96,7 +96,7 @@ class NegativeLogLikelihood(Metric):
             return values
 
 
-class GaussianNegativeLogLikelihood(Metric):
+class GaussianNegativeLogLikelihood(NegativeLogLikelihood):
     """The Gaussian Negative Log Likelihood Metric.
 
     Args:
@@ -127,36 +127,6 @@ class GaussianNegativeLogLikelihood(Metric):
             ``'none'`` or ``None``.
     """
 
-    is_differentiable: bool = False
-    higher_is_better: Optional[bool] = False
-    full_state_update: bool = False
-
-    def __init__(
-        self,
-        reduction: Literal["mean", "sum", "none", None] = "mean",
-        **kwargs: Any,
-    ) -> None:
-        super().__init__(**kwargs)
-
-        allowed_reduction = ("sum", "mean", "none", None)
-        if reduction not in allowed_reduction:
-            raise ValueError(
-                "Expected argument `reduction` to be one of ",
-                f"{allowed_reduction} but got {reduction}",
-            )
-
-        self.reduction = reduction
-
-        if self.reduction in ["mean", "sum"]:
-            self.add_state(
-                "values",
-                default=torch.tensor(0.0),
-                dist_reduce_fx="sum",
-            )
-        else:
-            self.add_state("values", default=[], dist_reduce_fx="cat")
-        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
-
     def update(
         self, probs: torch.Tensor, target: torch.Tensor, var: torch.Tensor
     ) -> None:
@@ -175,14 +145,3 @@ class GaussianNegativeLogLikelihood(Metric):
                 probs, target, var, reduction="sum"
             )
             self.total += target.size(0)
-
-    def compute(self) -> torch.Tensor:
-        """Computes NLL based on inputs passed in to ``update`` previously."""
-        values = dim_zero_cat(self.values)
-
-        if self.reduction == "sum":
-            return values.sum(dim=-1)
-        elif self.reduction == "mean":
-            return values.sum(dim=-1) / self.total
-        else:  # reduction is None or "none"
-            return values
