1e66ab3abe67593dc0a3dd26037ca8dd2d74816c
Olivier <olivier.ar.laurent@gmail.com>
2023-06-27 17:05:50 +0200

    :sparkles: Start supporting ensemble regression

--- torch_uncertainty/routines/regression.py
+++ torch_uncertainty/routines/regression.py
@@ -6,6 +6,7 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from einops import rearrange
 from pytorch_lightning.utilities.memory import get_model_size_mb
 from pytorch_lightning.utilities.types import EPOCH_OUTPUT, STEP_OUTPUT
 from torchmetrics import MeanSquaredError, MetricCollection
@@ -100,6 +101,7 @@ class RegressionSingle(pl.LightningModule):
             self.val_metrics.gnll.update(means, targets, vars)
         else:
             means = logits
+
         self.val_metrics.mse.update(means, targets)
 
     def validation_epoch_end(
@@ -121,6 +123,7 @@ class RegressionSingle(pl.LightningModule):
             self.test_reg_metrics.gnll.update(means, targets, vars)
         else:
             means = logits
+
         self.test_reg_metrics.mse.update(means, targets)
 
     def test_epoch_end(
@@ -135,4 +138,84 @@ class RegressionSingle(pl.LightningModule):
     def add_model_specific_args(
         parent_parser: ArgumentParser,
     ) -> ArgumentParser:
+        return parent_parser
+
+
+class RegressionEnsemble(RegressionSingle):
+    def __init__(
+        self,
+        num_classes: int,
+        model: nn.Module,
+        loss: nn.Module,
+        optimization_procedure: Any,
+        num_estimators: int,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            num_classes=num_classes,
+            model=model,
+            loss=loss,
+            optimization_procedure=optimization_procedure,
+            **kwargs,
+        )
+
+        self.num_estimators = num_estimators
+
+    def training_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> STEP_OUTPUT:
+        inputs, targets = batch
+
+        # eventual input repeat is done in the model
+        targets = targets.repeat(self.num_estimators)
+        return super().training_step((inputs, targets), batch_idx)
+
+    def validation_step(  # type: ignore
+        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> None:
+        inputs, targets = batch
+        logits = self.forward(inputs)
+        logits = rearrange(logits, "(m b) c -> b m c", m=self.num_estimators)
+        if self.binary_cls:
+            probs_per_est = torch.sigmoid(logits).squeeze(-1)
+        else:
+            probs_per_est = F.softmax(logits, dim=-1)
+
+        probs = probs_per_est.mean(dim=1)
+        self.val_cls_metrics.update(probs, targets)
+
+    def test_step(
+        self,
+        batch: Tuple[torch.Tensor, torch.Tensor],
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        inputs, targets = batch
+        logits = self.forward(inputs)
+        logits = rearrange(logits, "(n b) c -> b n c", n=self.num_estimators)
+
+        if self.dist_estimation:
+            means = logits[:, 0]
+            vars = F.softplus(logits[:, 1])
+            self.test_reg_metrics.gnll.update(means, targets, vars)
+        else:
+            means = logits
+
+        self.test_reg_metrics.mse.update(means, targets)
+
+    @staticmethod
+    def add_model_specific_args(
+        parent_parser: ArgumentParser,
+    ) -> ArgumentParser:
+        """Defines the routine's attributes via command-line options:
+
+        - ``--num_estimators``: sets :attr:`num_estimators`.
+        """
+        parent_parser = RegressionSingle.add_model_specific_args(parent_parser)
+        parent_parser.add_argument(
+            "--num_estimators",
+            type=int,
+            default=None,
+            help="Number of estimators for ensemble",
+        )
         return parent_parser
