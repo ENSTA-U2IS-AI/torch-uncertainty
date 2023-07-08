2f80a8fae96aaf33e6affed42025deb2aeab18f1
Olivier <olivier.ar.laurent@gmail.com>
2023-07-08 10:46:34 +0200

    :hammer: misc

--- experiments/classification/mnist/bayesian_lenet.py
+++ experiments/classification/mnist/bayesian_lenet.py
@@ -12,8 +12,6 @@ from torch_uncertainty.models.lenet import bayesian_lenet
 from torch_uncertainty.routines.classification import ClassificationSingle
 
 # fmt: on
-
-
 def optim_lenet(model: nn.Module) -> dict:
     """Optimization procedure for LeNet.
 
