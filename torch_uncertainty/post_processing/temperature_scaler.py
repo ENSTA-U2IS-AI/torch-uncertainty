39d77e2b4d228a922a652fc030b921414f8eacdf
Olivier <olivier.ar.laurent@gmail.com>
2023-07-13 13:37:19 +0200

    :sparkles: Improve temperature scaler

--- torch_uncertainty/post_processing/temperature_scaler.py
+++ torch_uncertainty/post_processing/temperature_scaler.py
@@ -1,7 +1,10 @@
 # fmt: off
+from typing import Literal, Optional
+
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 
 # fmt: on
@@ -12,6 +15,11 @@ class TemperatureScaler(nn.Module):
     Args:
         init_value (float, optional): Initial value for the temperature.
             Defaults to 1.
+        lr (float, optional): Learning rate for the optimizer. Defaults to 0.1.
+        max_iter (int, optional): Maximum number of iterations for the
+            optimizer. Defaults to 100.
+        device (Optional[Literal["cpu", "cuda"]], optional): Device to use
+            for optimization. Defaults to None.
 
     Reference:
         Guo, C., Pleiss, G., Sun, Y., & Weinberger, K. Q. On calibration
@@ -26,16 +34,18 @@ class TemperatureScaler(nn.Module):
     def __init__(
         self,
         init_val: float = 1,
-        lr: float = 0.01,
-        max_iter: int = 50,
-        device=None,
+        lr: float = 0.1,
+        max_iter: int = 100,
+        device: Optional[Literal["cpu", "cuda"]] = None,
     ) -> None:
         super().__init__()
         self.device = device
         if init_val <= 0:
             raise ValueError("Initial temperature value must be positive.")
 
-        self.temperature = nn.Parameter(torch.ones(1) * init_val).to(device)
+        self.temperature = nn.Parameter(
+            torch.ones(1, device=device) * init_val, requires_grad=True
+        )
         self.criterion = nn.CrossEntropyLoss()
 
         if lr <= 0:
@@ -56,17 +66,27 @@ class TemperatureScaler(nn.Module):
         if val <= 0:
             raise ValueError("Temperature value must be positive.")
 
-        self.temperature = nn.Parameter(torch.ones(1) * val)
+        self.temperature = nn.Parameter(
+            torch.ones(1, device=self.device) * val, requires_grad=True
+        )
 
     def fit(
-        self, model: nn.Module, val_loader: DataLoader
+        self,
+        model: nn.Module,
+        calib_loader: DataLoader,
+        save_logits: bool = False,
+        progress: bool = True,
     ) -> "TemperatureScaler":
         """
         Fit the temperature to the validation data.
 
         Args:
             model (nn.Module): Model to calibrate.
-            val_loader (DataLoader): Validation dataloader.
+            calib_loader (DataLoader): Calibration dataloader.
+            save_logits (bool, optional): Whether to save the logits and
+                labels. Defaults to False.
+            progress (bool, optional): Whether to show a progress bar.
+                Defaults to True.
 
         Returns:
             TemperatureScaler: Calibrated scaler.
@@ -74,13 +94,13 @@ class TemperatureScaler(nn.Module):
         logits_list = []
         labels_list = []
         with torch.no_grad():
-            for input, label in val_loader:
+            for input, label in tqdm(calib_loader, disable=not progress):
                 input = input.to(self.device)
                 logits = model(input)
                 logits_list.append(logits)
                 labels_list.append(label)
-            logits = torch.cat(logits_list).to(self.device)
-            labels = torch.cat(labels_list).to(self.device)
+        logits = torch.cat(logits_list).detach().to(self.device)
+        labels = torch.cat(labels_list).detach().to(self.device)
 
         optimizer = optim.LBFGS(
             [self.temperature], lr=self.lr, max_iter=self.max_iter
@@ -94,23 +114,26 @@ class TemperatureScaler(nn.Module):
 
         optimizer.step(eval)
         self.trained = True
-
+        if save_logits:
+            self.logits = logits
+            self.labels = labels
         return self
 
     def forward(self, logits: torch.Tensor) -> torch.Tensor:
-        if not self.trained:
-            print(
-                "TemperatureScaler has not been trained yet. Returning a "
-                "manually tempered input."
-            )
-        return self._scale(logits)
+        with torch.no_grad():
+            if not self.trained:
+                print(
+                    "TemperatureScaler has not been trained yet. Returning a "
+                    "manually tempered input."
+                )
+            return self._scale(logits)
 
     def _scale(self, logits: torch.Tensor) -> torch.Tensor:
         """
-        Scale the logits by the temperature.
+        Scale the logits with the optimal temperature.
 
         Args:
-            logits (torch.Tensor): Logits to scale.
+            logits (torch.Tensor): Logits to be scaled.
 
         Returns:
             torch.Tensor: Scaled logits.
@@ -119,3 +142,10 @@ class TemperatureScaler(nn.Module):
             logits.size(0), logits.size(1)
         )
         return logits / temperature
+
+    def fit_predict(
+        self, model: nn.Module, calib_loader: DataLoader, progress: bool = True
+    ) -> torch.Tensor:
+        self.fit(model, calib_loader, save_logits=True, progress=progress)
+        calib_logits = self(self.logits)
+        return calib_logits
