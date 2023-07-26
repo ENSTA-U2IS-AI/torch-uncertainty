7ed7bf7a5b1d0a978a892ba38ddac0673fe8568a
Olivier <olivier.ar.laurent@gmail.com>
2023-07-26 10:12:40 +0200

    :zap: Update version for next release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="torch-uncertainty",
-    version="0.1.3",
+    version="0.1.4",
     description="A PyTorch Library for benchmarking and leveraging efficient"
     "predictive uncertainty quantification techniques.",
     author="Adrien Lafage & Olivier Laurent",
