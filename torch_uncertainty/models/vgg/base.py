c25a61f57db9c4be8b600584d8bf574eae26c042
Olivier <olivier.ar.laurent@gmail.com>
2023-08-25 14:19:43 +0200

    :ok_hand: Fix E261

--- torch_uncertainty/models/vgg/base.py
+++ torch_uncertainty/models/vgg/base.py
@@ -111,15 +111,15 @@ class VGG(nn.Module):
                 nn.init.kaiming_normal_(
                     m.weight, mode="fan_out", nonlinearity="relu"
                 )
-                if m.bias is not None: # coverage: ignore
+                if m.bias is not None:  # coverage: ignore
                     nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
-                if m.bias is not None: # coverage: ignore
+                if m.bias is not None:  # coverage: ignore
                     nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.Linear) or isinstance(m, PackedLinear):
                 nn.init.normal_(m.weight, 0, 0.01)
-                if m.bias is not None: # coverage: ignore
+                if m.bias is not None:  # coverage: ignore
                     nn.init.constant_(m.bias, 0)
 
 
