688ec10e3bf97a8c98014b6b0a1a756912df411e
Olivier <olivier.ar.laurent@gmail.com>
2023-09-16 13:02:22 +0200

    :bug: Fix WideResNet training script

--- experiments/classification/cifar100/wideresnet.py
+++ experiments/classification/cifar100/wideresnet.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     else:
         root = Path(args.root)
 
-    net_name = f"{args.version}-wideresnet{args.arch}-cifar10"
+    net_name = f"{args.version}-wideresnet28x10-cifar100"
 
     # datamodule
     args.root = str(root / "data")
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         in_channels=dm.num_channels,
         loss=nn.CrossEntropyLoss,
         optimization_procedure=get_procedure(
-            f"resnet{args.arch}", "cifar100", args.version
+            f"wideresnet28x10", "cifar100", args.version
         ),
         style="cifar",
         **vars(args),
