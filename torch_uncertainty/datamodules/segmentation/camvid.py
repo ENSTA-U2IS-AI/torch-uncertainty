0158cdd4351af402dc967f46e1db6e453141624e
alafage <adrienlafage@outlook.com>
2023-12-13 17:49:41 +0100

    :construction: Init CamVid DataModule class

---
+++ torch_uncertainty/datamodules/segmentation/camvid.py
@@ -0,0 +1,69 @@
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Any
+
+from torch_uncertainty.datamodules.abstract import AbstractDataModule
+from torch_uncertainty.datasets.segmentation import CamVid
+
+
+class CamVidDataModule(AbstractDataModule):
+    def __init__(
+        self,
+        root: str | Path,
+        batch_size: int,
+        num_workers: int = 1,
+        pin_memory: bool = True,
+        persistent_workers: bool = True,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            root=root,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            persistent_workers=persistent_workers,
+        )
+
+        self.dataset = CamVid
+
+        self.transform_train = ...
+        self.transform_test = ...
+
+    def prepare_data(self) -> None:  # coverage: ignore
+        self.dataset(root=self.root, download=True)
+
+    def setup(self, stage: str | None = None) -> None:
+        if stage == "fit" or stage is None:
+            self.train = self.dataset(
+                root=self.root,
+                split="train",
+                download=False,
+                transform=self.transform_train,
+            )
+            self.val = self.dataset(
+                root=self.root,
+                split="val",
+                download=False,
+                transform=self.transform_test,
+            )
+        elif stage == "test":
+            self.test = self.dataset(
+                root=self.root,
+                split="test",
+                download=False,
+                transform=self.transform_test,
+            )
+        else:
+            raise ValueError(f"Stage {stage} is not supported.")
+
+    @classmethod
+    def add_argparse_args(
+        cls,
+        parent_parser: ArgumentParser,
+        **kwargs: Any,
+    ) -> ArgumentParser:
+        p = parent_parser.add_argument_group("datamodule")
+        p.add_argument("--root", type=str, default="./data/")
+        p.add_argument("--batch_size", type=int, default=128)
+        p.add_argument("--num_workers", type=int, default=4)
+        return parent_parser
