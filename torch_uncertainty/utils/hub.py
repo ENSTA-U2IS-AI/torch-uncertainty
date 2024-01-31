d338df355b58194d083beb8ebb5fafed00f9d1d1
Olivier <olivier.ar.laurent@gmail.com>
2024-01-31 23:20:21 +0100

    :sparkles: Enable loading safetensors

--- torch_uncertainty/utils/hub.py
+++ torch_uncertainty/utils/hub.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import torch
 import yaml
 from huggingface_hub import hf_hub_download
+from huggingface_hub.utils._errors import EntryNotFoundError
+from safetensors import safe_open
 
 
 def load_hf(weight_id: str, version: int = 0) -> tuple[torch.Tensor, dict]:
@@ -21,13 +23,25 @@ def load_hf(weight_id: str, version: int = 0) -> tuple[torch.Tensor, dict]:
     repo_id = f"torch-uncertainty/{weight_id}"
 
     # Load the weights
+    pickle = True
     if version is None or version == 0:
         filename = f"{weight_id}.ckpt"
     else:
         filename = f"{weight_id}_{version}.ckpt"
+    try:
+        weight_path = hf_hub_download(repo_id=repo_id, filename=filename)
+    except EntryNotFoundError:
+        pickle = False
+        filename = f"{weight_id}.safetensors"
+        weight_path = hf_hub_download(repo_id=repo_id, filename=filename)
+    if pickle:
+        weight = torch.load(weight_path, map_location=torch.device("cpu"))
+    else:
+        weight = {}
+        with safe_open("model.safetensors", framework="pt", device="cpu") as f:
+            for key in f:
+                weight[key] = f.get_tensor(key)
 
-    weight_path = hf_hub_download(repo_id=repo_id, filename=filename)
-    weight = torch.load(weight_path, map_location=torch.device("cpu"))
     if "state_dict" in weight:  # coverage: ignore
         weight = weight["state_dict"]
 
