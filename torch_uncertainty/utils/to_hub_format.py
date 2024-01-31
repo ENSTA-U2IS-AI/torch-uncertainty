f40a436b70e2c3c5098ba9211092f4319ab6b9bc
Olivier <olivier.ar.laurent@gmail.com>
2024-01-31 23:17:17 +0100

    :sparkles: Enable saving ckpts to fp16

--- torch_uncertainty/utils/to_hub_format.py
+++ torch_uncertainty/utils/to_hub_format.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import torch
 from safetensors.torch import save_file
 
+torch.set_grad_enabled(False)
+
 parser = argparse.ArgumentParser(
     prog="to_hub_format",
     description="Post-process the checkpoints before the upload to HuggingFace",
@@ -22,14 +24,21 @@ parser.add_argument(
 parser.add_argument(
     "--safe", action="store_true", help="whether to use safetensors"
 )
+parser.add_argument(
+    "--fp16", action="store_true", help="whether to use fp16 for the checkpoint"
+)
 
 args = parser.parse_args()
 
 if not args.path.exists():
     raise ValueError("File does not exist")
 
+dtype = torch.float16 if args.fp16 else torch.float32
 model = torch.load(args.path)["state_dict"]
-model = {key.replace("model.", ""): val.cpu() for key, val in model.items()}
+model = {
+    key.replace("model.", ""): val.to(device="cpu", dtype=dtype)
+    for key, val in model.items()
+}
 
 output_name = args.name
 if args.version != 0:
