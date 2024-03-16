bbbcb7c8fcace7d0e285e1bd9c0e58a84c5f8385
Olivier <olivier.ar.laurent@gmail.com>
2024-03-16 16:19:23 +0100

    :sparkles: Enable ensembles in regression routine

--- torch_uncertainty/routines/regression.py
+++ torch_uncertainty/routines/regression.py
@@ -1,37 +1,63 @@
+import torch
 from lightning.pytorch import LightningModule
 from lightning.pytorch.utilities.types import STEP_OUTPUT
 from torch import Tensor, nn
-from torchmetrics import MeanSquaredError, MetricCollection
+from torch.distributions import (
+    Categorical,
+    Independent,
+    MixtureSameFamily,
+)
+from torchmetrics import MeanAbsoluteError, MeanSquaredError, MetricCollection
 
 from torch_uncertainty.metrics.nll import DistributionNLL
+from torch_uncertainty.utils.distributions import to_ens_dist
 
 
 class RegressionRoutine(LightningModule):
     def __init__(
         self,
-        num_features: int,
+        num_outputs: int,
         model: nn.Module,
         loss: type[nn.Module],
-        num_estimators: int,
+        num_estimators: int = 1,
         format_batch_fn: nn.Module | None = None,
     ) -> None:
         super().__init__()
 
         self.model = model
         self.loss = loss
+
+        if format_batch_fn is None:
+            format_batch_fn = nn.Identity()
+
         self.format_batch_fn = format_batch_fn
 
         reg_metrics = MetricCollection(
             {
-                "mse": MeanSquaredError(squared=True),
-                "nll": DistributionNLL(),
+                "mae": MeanAbsoluteError(),
+                "mse": MeanSquaredError(squared=False),
+                "nll": DistributionNLL(reduction="mean"),
             },
             compute_groups=False,
         )
         self.val_metrics = reg_metrics.clone(prefix="reg_val/")
         self.test_metrics = reg_metrics.clone(prefix="reg_test/")
 
+        if num_estimators < 1:
+            raise ValueError(
+                f"num_estimators must be positive, got {num_estimators}."
+            )
         self.num_estimators = num_estimators
+
+        if num_outputs < 1:
+            raise ValueError(
+                f"num_outputs must be positive, got {num_outputs}."
+            )
+        self.num_outputs = num_outputs
+
+        self.one_dim_regression = False
+        if num_outputs == 1:
+            self.one_dim_regression = True
 
     def on_train_start(self) -> None:
         # hyperparameters for performances
@@ -54,14 +80,14 @@ class RegressionRoutine(LightningModule):
     def training_step(
         self, batch: tuple[Tensor, Tensor], batch_idx: int
     ) -> STEP_OUTPUT:
-        inputs, targets = batch
+        inputs, targets = self.format_batch_fn(batch)
 
-        # eventual input repeat is done in the model
-        targets = targets.repeat((self.num_estimators, 1))
+        dists = self.forward(inputs)
 
-        logits = self.forward(inputs)
+        if self.one_dim_regression:
+            targets = targets.unsqueeze(-1)
 
-        loss = self.criterion(logits, targets)
+        loss = self.criterion(dists, targets)
 
         self.log("train_loss", loss)
         return loss
@@ -73,8 +99,18 @@ class RegressionRoutine(LightningModule):
 
         dists = self.forward(inputs)
 
-        self.val_metrics.mse.update(dists.loc, targets)
-        self.val_metrics.nll.update(dists, targets)
+        ens_dist = Independent(
+            to_ens_dist(dists, num_estimators=self.num_estimators), 1
+        )
+        mix = Categorical(torch.ones(self.num_estimators, device=self.device))
+        mixture = MixtureSameFamily(mix, ens_dist)
+
+        if self.one_dim_regression:
+            targets = targets.unsqueeze(-1)
+
+        self.val_metrics.mse.update(mixture.mean, targets)
+        self.val_metrics.mae.update(mixture.mean, targets)
+        self.val_metrics.nll.update(mixture, targets)
 
     def on_validation_epoch_end(self) -> None:
         self.log_dict(self.val_metrics.compute())
@@ -94,9 +130,19 @@ class RegressionRoutine(LightningModule):
 
         inputs, targets = batch
         dists = self.forward(inputs)
+        ens_dist = Independent(
+            to_ens_dist(dists, num_estimators=self.num_estimators), 1
+        )
 
-        self.test_metrics.mse.update(dists.loc, targets)
-        self.test_metrics.nll.update(dists, targets)
+        mix = Categorical(torch.ones(self.num_estimators, device=self.device))
+        mixture = MixtureSameFamily(mix, ens_dist)
+
+        if self.one_dim_regression:
+            targets = targets.unsqueeze(-1)
+
+        self.test_metrics.mae.update(mixture.mean, targets)
+        self.test_metrics.mse.update(mixture.mean, targets)
+        self.test_metrics.nll.update(mixture, targets)
 
     def on_test_epoch_end(self) -> None:
         self.log_dict(
