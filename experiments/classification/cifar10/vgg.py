9d3aff0a813642f53e2a49cecbfba594001b1441
alafage <adrienlafage@outlook.com>
2024-03-18 16:31:26 +0100

    :bug: Fix fast_dev_run raising error

--- experiments/classification/cifar10/vgg.py
+++ experiments/classification/cifar10/vgg.py
@@ -20,5 +20,9 @@ def cli_main() -> ResNetCLI:
 if __name__ == "__main__":
     torch.set_float32_matmul_precision("medium")
     cli = cli_main()
-    if cli.subcommand == "fit" and cli._get(cli.config, "eval_after_fit"):
+    if (
+        (not cli.trainer.fast_dev_run)
+        and cli.subcommand == "fit"
+        and cli._get(cli.config, "eval_after_fit")
+    ):
         cli.trainer.test(datamodule=cli.datamodule, ckpt_path="best")
