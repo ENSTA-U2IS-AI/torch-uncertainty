1af083c9b3d3f85e000ac79e2d2bd09f0ba9c062
Olivier <olivier.ar.laurent@gmail.com>
2024-03-21 11:32:39 +0100

    :wrapped_gift: Add MLP regression configs

---
+++ experiments/regression/uci_datasets/mlp.py
@@ -0,0 +1,26 @@
+import torch
+from lightning.pytorch.cli import LightningArgumentParser
+
+from torch_uncertainty.baselines.regression import MLPBaseline
+from torch_uncertainty.datamodules import UCIDataModule
+from torch_uncertainty.utils import TULightningCLI
+
+
+class MLPCLI(TULightningCLI):
+    def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
+        parser.add_optimizer_args(torch.optim.Adam)
+
+
+def cli_main() -> MLPCLI:
+    return MLPCLI(MLPBaseline, UCIDataModule)
+
+
+if __name__ == "__main__":
+    torch.set_float32_matmul_precision("medium")
+    cli = cli_main()
+    if (
+        (not cli.trainer.fast_dev_run)
+        and cli.subcommand == "fit"
+        and cli._get(cli.config, "eval_after_fit")
+    ):
+        cli.trainer.test(datamodule=cli.datamodule, ckpt_path="best")
