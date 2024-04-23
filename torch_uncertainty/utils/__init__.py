96bd269e1c571810153b33d3c994eb2cfe2321cf
Olivier <olivier.ar.laurent@gmail.com>
2024-04-23 14:11:26 +0200

    :sparkles: Enable interpolation choice on ImageNet

--- torch_uncertainty/utils/__init__.py
+++ torch_uncertainty/utils/__init__.py
@@ -4,3 +4,4 @@ from .cli import TULightningCLI
 from .hub import load_hf
 from .misc import create_train_val_split, csv_writer, plot_hist
 from .trainer import TUTrainer
+from .transforms import interpolation_modes_from_str
