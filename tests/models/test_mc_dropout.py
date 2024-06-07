8655becd5e8c707cc31b0b66398c3d0ce02f3c86
Olivier <olivier.ar.laurent@gmail.com>
2024-06-07 10:36:00 +0200

    :bug: Fix MC Dropout test

--- tests/models/test_mc_dropout.py
+++ tests/models/test_mc_dropout.py
@@ -27,14 +27,23 @@ class TestMCDropout:
         assert not dropout_model.training
         dropout_model(torch.rand(1, 10))
 
+        dropout_model = mc_dropout(model, num_estimators=5, on_batch=False)
+        dropout_model.eval()
+        assert not dropout_model.training
+        dropout_model(torch.rand(1, 10))
+
     def test_mc_dropout_errors(self):
         model = dummy_model(10, 5, 0.1)
 
         with pytest.raises(ValueError):
-            _MCDropout(model=model, num_estimators=-1, last_layer=True)
+            _MCDropout(
+                model=model, num_estimators=-1, last_layer=True, on_batch=True
+            )
 
         with pytest.raises(ValueError):
-            _MCDropout(model=model, num_estimators=0, last_layer=False)
+            _MCDropout(
+                model=model, num_estimators=0, last_layer=False, on_batch=False
+            )
 
         dropout_model = mc_dropout(model, 5)
         with pytest.raises(TypeError):
