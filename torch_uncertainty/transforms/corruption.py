6d4400f30b76e68919920e30cac0c8b0a21c180e
Olivier <olivier.ar.laurent@gmail.com>
2024-10-21 11:59:31 +0200

    :bug: Fix most corruption transforms

--- torch_uncertainty/transforms/corruption.py
+++ torch_uncertainty/transforms/corruption.py
@@ -1,5 +1,6 @@
 """Adapted from https://github.com/hendrycks/robustness."""
 
+import ctypes
 from importlib import util
 from io import BytesIO
 
@@ -59,6 +60,7 @@ __all__ = [
     "JPEGCompression",
     "GaussianBlur",
     "SpeckleNoise",
+    "Saturation",
 ]
 
 
@@ -147,8 +149,8 @@ class DefocusBlur(TUCorruption):
                 "Please install torch_uncertainty with the image option:"
                 """pip install -U "torch_uncertainty[image]"."""
             )
-        self.radius = [0.3, 0.4, 0.5, 1, 1.5][severity - 1]
-        self.alias_blur = [0.4, 0.5, 0.6, 0.2, 0.1][severity - 1]
+        self.radius = [3, 4, 6, 8, 10][severity - 1]
+        self.alias_blur = [0.1, 0.5, 0.5, 0.5, 0.5][severity - 1]
 
     def forward(self, img: Tensor) -> Tensor:
         if self.severity == 0:
@@ -214,6 +216,15 @@ def disk(radius: int, alias_blur: float = 0.1, dtype=np.float32):
     return cv2.GaussianBlur(aliased_disk, ksize=ksize, sigmaX=alias_blur)
 
 
+# Tell Python about the C method
+wandlibrary.MagickMotionBlurImage.argtypes = (
+    ctypes.c_void_p,  # wand
+    ctypes.c_double,  # radius
+    ctypes.c_double,  # sigma
+    ctypes.c_double,
+)  # angle
+
+
 class MotionImage(WandImage):
     def motion_blur(self, radius=0.0, sigma=0.0, angle=0.0):
         wandlibrary.MagickMotionBlurImage(self.wand, radius, sigma, angle)
@@ -223,18 +234,28 @@ class MotionBlur(TUCorruption):
     def __init__(self, severity: int) -> None:
         super().__init__(severity)
         self.rng = np.random.default_rng()
+        self.radius = [10, 15, 15, 15, 20][severity - 1]
+        self.sigma = [3, 5, 8, 12, 15][severity - 1]
+        self.to_pil_img = ToPILImage()
+        self.to_tensor = ToTensor()
 
     def forward(self, img: Tensor) -> Tensor:
         if self.severity == 0:
             return img
         output = BytesIO()
-        ToPILImage()(img).save(output, "PNG")
+        pil_img = self.to_pil_img(img)
+        pil_img.save(output, "PNG")
         x = MotionImage(blob=output.getvalue())
-        x.motion_blur(radius=10, sigma=3, angle=self.rng.uniform(-45, 45))
+        x.motion_blur(
+            radius=self.radius,
+            sigma=self.sigma,
+            angle=self.rng.uniform(-45, 45),
+        )
         x = cv2.imdecode(
             np.fromstring(x.make_blob(), np.uint8), cv2.IMREAD_UNCHANGED
         )
-        return torch.clip(torch.as_tensor(x)[..., [2, 1, 0]], 0, 255) / 255
+        x = np.clip(x[..., [2, 1, 0]], 0, 255)
+        return self.to_tensor(x)
 
 
 def clipped_zoom(img, zoom_factor):
@@ -279,7 +300,7 @@ class ZoomBlur(TUCorruption):
 class Snow(TUCorruption):
     def __init__(self, severity: int) -> None:
         super().__init__(severity)
-        self.snow_layers = [
+        self.mix = [
             (0.1, 0.3, 3, 0.5, 10, 4, 0.8),
             (0.2, 0.3, 2, 0.5, 12, 4, 0.7),
             (0.55, 0.3, 4, 0.9, 12, 8, 0.7),
@@ -294,12 +315,10 @@ class Snow(TUCorruption):
         _, height, width = img.shape
         x = img.numpy()
         snow_layer = self.rng.normal(
-            size=x.shape[:2], loc=self.snow_layers[0], scale=self.snow_layers[1]
-        )
-        snow_layer = clipped_zoom(
-            snow_layer[..., np.newaxis], self.snow_layers[2]
-        )
-        snow_layer[snow_layer < self.snow_layers[3]] = 0
+            size=x.shape[1:], loc=self.mix[0], scale=self.mix[1]
+        )[..., np.newaxis]
+        snow_layer = clipped_zoom(snow_layer, self.mix[2])
+        snow_layer[snow_layer < self.mix[3]] = 0
         snow_layer = Image.fromarray(
             (np.clip(snow_layer.squeeze(), 0, 1) * 255).astype(np.uint8),
             mode="L",
@@ -308,8 +327,8 @@ class Snow(TUCorruption):
         snow_layer.save(output, format="PNG")
         snow_layer = MotionImage(blob=output.getvalue())
         snow_layer.motion_blur(
-            radius=self.snow_layers[4],
-            sigma=self.snow_layers[5],
+            radius=self.mix[4],
+            sigma=self.mix[5],
             angle=self.rng.uniform(-135, -45),
         )
         snow_layer = (
@@ -319,10 +338,13 @@ class Snow(TUCorruption):
             )
             / 255.0
         )
-        snow_layer = snow_layer[..., np.newaxis]
-        x = self.snow_layers[6] * x + (1 - self.snow_layers[6]) * np.maximum(
+        snow_layer = snow_layer[np.newaxis, ...]
+        x = self.mix[6] * x + (1 - self.mix[6]) * np.maximum(
             x,
-            cv2.cvtColor(x, cv2.COLOR_RGB2GRAY).reshape(height, width, 1) * 1.5
+            cv2.cvtColor(x.transpose([1, 2, 0]), cv2.COLOR_RGB2GRAY).reshape(
+                1, height, width
+            )
+            * 1.5
             + 0.5,
         )
         return torch.clip(
@@ -351,49 +373,113 @@ class Frost(TUCorruption):
         return torch.clip(self.mix[0] * img + self.mix[1] * frost_img, 0, 1)
 
 
+def plasma_fractal(height, width, wibbledecay=3):
+    """Generate a heightmap using diamond-square algorithm.
+    Return square 2d array, side length 'mapsize', of floats in range 0-1.
+    'mapsize' must be a power of two.
+    """
+    maparray = np.empty((height, width), dtype=np.float64)
+    maparray[0, 0] = 0
+    stepsize = height
+    wibble = 100
+    rng = np.random.default_rng()
+
+    def wibbledmean(array):
+        return array / 4 + wibble * rng.uniform(-wibble, wibble, array.shape)
+
+    def fillsquares():
+        """For each square of points stepsize apart, calculate middle value as mean of points + wibble."""
+        cornerref = maparray[0:height:stepsize, 0:height:stepsize]
+        squareaccum = cornerref + np.roll(cornerref, shift=-1, axis=0)
+        squareaccum += np.roll(squareaccum, shift=-1, axis=1)
+        maparray[
+            stepsize // 2 : height : stepsize,
+            stepsize // 2 : height : stepsize,
+        ] = wibbledmean(squareaccum)
+
+    def filldiamonds():
+        """For each diamond of points stepsize apart, calculate middle value as mean of points + wibble."""
+        mapsize = maparray.shape[0]
+        drgrid = maparray[
+            stepsize // 2 : mapsize : stepsize,
+            stepsize // 2 : mapsize : stepsize,
+        ]
+        ulgrid = maparray[0:mapsize:stepsize, 0:mapsize:stepsize]
+        ldrsum = drgrid + np.roll(drgrid, 1, axis=0)
+        lulsum = ulgrid + np.roll(ulgrid, -1, axis=1)
+        ltsum = ldrsum + lulsum
+        maparray[0:mapsize:stepsize, stepsize // 2 : mapsize : stepsize] = (
+            wibbledmean(ltsum)
+        )
+        tdrsum = drgrid + np.roll(drgrid, 1, axis=1)
+        tulsum = ulgrid + np.roll(ulgrid, -1, axis=0)
+        ttsum = tdrsum + tulsum
+        maparray[stepsize // 2 : mapsize : stepsize, 0:mapsize:stepsize] = (
+            wibbledmean(ttsum)
+        )
+
+    while stepsize >= 2:
+        fillsquares()
+        filldiamonds()
+        stepsize //= 2
+        wibble /= wibbledecay
+
+    maparray -= maparray.min()
+    return maparray / maparray.max()
+
+
 class Fog(TUCorruption):
-    def __init__(self, severity: int) -> None:
+    def __init__(self, severity: int, size: int = 256) -> None:
         super().__init__(severity)
+        if (size & (size - 1) == 0) and size != 0:
+            self.size = size
+            self.resize = Resize((size, size), InterpolationMode.BICUBIC)
+        else:
+            raise ValueError(f"Size must be a power of 2. Got {size}.")
         self.mix = [(1.5, 2), (2, 2), (2.5, 1.7), (2.5, 1.5), (3, 1.4)][
             severity - 1
         ]
 
     def forward(self, img: Tensor) -> Tensor:
-        _, height, width = img.shape
         if self.severity == 0:
             return img
+        _, height, width = img.shape
+        if height != width:
+            raise ValueError(f"Image must be square. Got {height}x{width}.")
+        img = self.resize(img)
         max_val = img.max()
-        img += (
+        fog = (
             self.mix[0]
-            * plasma_fractal(wibbledecay=self.mix[1])[:height, :width][
-                ..., np.newaxis
-            ]
+            * plasma_fractal(
+                height=height, width=width, wibbledecay=self.mix[1]
+            )[:height, :width]
         )
-        return torch.clip(img * max_val / (max_val + self.mix[0]), 0, 1)
+        final = torch.clip(
+            (img + fog) * max_val / (max_val + self.mix[0]), 0, 1
+        )
+        return Resize((height, width), InterpolationMode.BICUBIC)(final)
 
 
-class Brightness(TUCorruption, IBrightness):
+class Brightness(IBrightness, TUCorruption):
     def __init__(self, severity: int) -> None:
-        super(TUCorruption, self).__init__(severity)
-        super(IBrightness, self).__init__()
-        self.level = [0.1, 0.2, 0.3, 0.4, 0.5][severity - 1]
+        TUCorruption.__init__(self, severity)
+        self.level = [1.1, 1.2, 1.3, 1.4, 1.5][severity - 1]
 
     def forward(self, img: Tensor) -> Tensor:
         if self.severity == 0:
             return img
-        return super(IBrightness, self).forward(img, self.level)
+        return IBrightness.forward(self, img, self.level)
 
 
-class Contrast(TUCorruption, IContrast):
+class Contrast(IContrast, TUCorruption):
     def __init__(self, severity: int) -> None:
-        super(TUCorruption, self).__init__(severity)
-        super(IContrast, self).__init__()
+        TUCorruption.__init__(self, severity)
         self.level = [0.4, 0.3, 0.2, 0.1, 0.05][severity - 1]
 
-    def forward(self, img: Tensor) -> Tensor:
+    def forward(self, img: Tensor) -> Tensor | Image.Image:
         if self.severity == 0:
             return img
-        return super(IContrast, self).forward(img, self.level)
+        return IContrast.forward(self, img, self.level)
 
 
 class Pixelate(TUCorruption):
@@ -434,26 +520,27 @@ class Elastic(TUCorruption):
                 "Please install torch_uncertainty with the image option:"
                 """pip install -U "torch_uncertainty[image]"."""
             )
-        # Check with different sizes
         self.mixing = [
             (
-                244 * 2,
-                244 * 0.7,
-                244 * 0.1,
-            ),  # 244 should have been 224, but ultimately nothing is incorrect
-            (244 * 2, 244 * 0.08, 244 * 0.2),
-            (244 * 0.05, 244 * 0.01, 244 * 0.02),
-            (244 * 0.07, 244 * 0.01, 244 * 0.02),
-            (244 * 0.12, 244 * 0.01, 244 * 0.02),
+                2,
+                0.7,
+                0.1,
+            ),
+            (2, 0.08, 0.2),
+            (0.05, 0.01, 0.02),
+            (0.07, 0.01, 0.02),
+            (0.12, 0.01, 0.02),
         ][severity - 1]
         self.rng = np.random.default_rng()
 
     def forward(self, img: Tensor) -> Tensor:
+        if self.severity == 0:
+            return img
         shape = img.shape
         shape_size = shape[1:]
-        center_square = np.float32(shape_size) // 2
+        center_square = torch.tensor([shape_size[0] // 2, shape_size[1] // 2])
         square_size = min(shape_size) // 3
-        pts1 = np.float32(
+        pts1 = np.array(
             [
                 center_square + square_size,
                 [
@@ -461,20 +548,25 @@ class Elastic(TUCorruption):
                     center_square[1] - square_size,
                 ],
                 center_square - square_size,
-            ]
-        )
-        pts2 = pts1 + self.rng.uniform(
-            -self.mixing[2], self.mixing[2], size=pts1.shape
-        ).astype(np.float32)
-        m = cv2.getAffineTransform(pts1, pts2)
-        img = cv2.warpAffine(
-            img, m, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101
+            ],
+            dtype=np.float32,
         )
 
+        pts2 = pts1 + self.rng.uniform(
+            -self.mixing[2] * shape_size[0],
+            self.mixing[2] * shape_size[0],
+            size=pts1.shape,
+        ).astype(np.float32)
+        img = cv2.warpAffine(
+            img.numpy(),
+            cv2.getAffineTransform(pts1, pts2),
+            shape_size[::-1],
+            borderMode=cv2.BORDER_REFLECT_101,
+        )
         dx = (
             gaussian(
-                self.rng.uniform(-1, 1, size=shape[:2]),
-                self.mixing[1],
+                self.rng.uniform(-1, 1, size=shape[1:]),
+                self.mixing[1] * shape_size[0],
                 mode="reflect",
                 truncate=3,
             )
@@ -482,7 +574,7 @@ class Elastic(TUCorruption):
         ).astype(np.float32)
         dy = (
             gaussian(
-                self.rng.uniform(-1, 1, size=shape[:2]),
+                self.rng.uniform(-1, 1, size=shape[1:]),
                 self.mixing[1],
                 mode="reflect",
                 truncate=3,
@@ -491,79 +583,22 @@ class Elastic(TUCorruption):
         ).astype(np.float32)
         dx, dy = dx[..., np.newaxis], dy[..., np.newaxis]
         x, y, z = np.meshgrid(
-            np.arange(shape[2]), np.arange(shape[1]), np.arange(shape[0])
+            np.arange(shape[1]), np.arange(shape[0]), np.arange(shape[2])
         )
         indices = (
             np.reshape(y + dy, (-1, 1)),
             np.reshape(x + dx, (-1, 1)),
             np.reshape(z, (-1, 1)),
         )
-        return torch.as_tensor(
-            np.clip(
-                map_coordinates(img, indices, order=1, mode="reflect").reshape(
-                    shape
-                ),
-                0,
-                1,
-            )
+        final = np.clip(
+            map_coordinates(img, indices, order=1, mode="reflect").reshape(
+                shape
+            ),
+            0,
+            1,
         )
-
-
-# modification of https://github.com/FLHerne/mapgen/blob/master/diamondsquare.py
-def plasma_fractal(mapsize=256, wibbledecay=3):
-    """Generate a heightmap using diamond-square algorithm.
-    Return square 2d array, side length 'mapsize', of floats in range 0-255.
-    'mapsize' must be a power of two.
-    """
-    assert mapsize & (mapsize - 1) == 0
-    maparray = np.empty((mapsize, mapsize), dtype=np.float64)
-    maparray[0, 0] = 0
-    stepsize = mapsize
-    wibble = 100
-    rng = np.random.default_rng()
-
-    def wibbledmean(array):
-        return array / 4 + wibble * rng.uniform(-wibble, wibble, array.shape)
-
-    def fillsquares():
-        """For each square of points stepsize apart, calculate middle value as mean of points + wibble."""
-        cornerref = maparray[0:mapsize:stepsize, 0:mapsize:stepsize]
-        squareaccum = cornerref + np.roll(cornerref, shift=-1, axis=0)
-        squareaccum += np.roll(squareaccum, shift=-1, axis=1)
-        maparray[
-            stepsize // 2 : mapsize : stepsize,
-            stepsize // 2 : mapsize : stepsize,
-        ] = wibbledmean(squareaccum)
-
-    def filldiamonds():
-        """For each diamond of points stepsize apart, calculate middle value as mean of points + wibble."""
-        mapsize = maparray.shape[0]
-        drgrid = maparray[
-            stepsize // 2 : mapsize : stepsize,
-            stepsize // 2 : mapsize : stepsize,
-        ]
-        ulgrid = maparray[0:mapsize:stepsize, 0:mapsize:stepsize]
-        ldrsum = drgrid + np.roll(drgrid, 1, axis=0)
-        lulsum = ulgrid + np.roll(ulgrid, -1, axis=1)
-        ltsum = ldrsum + lulsum
-        maparray[0:mapsize:stepsize, stepsize // 2 : mapsize : stepsize] = (
-            wibbledmean(ltsum)
-        )
-        tdrsum = drgrid + np.roll(drgrid, 1, axis=1)
-        tulsum = ulgrid + np.roll(ulgrid, -1, axis=0)
-        ttsum = tdrsum + tulsum
-        maparray[stepsize // 2 : mapsize : stepsize, 0:mapsize:stepsize] = (
-            wibbledmean(ttsum)
-        )
-
-    while stepsize >= 2:
-        fillsquares()
-        filldiamonds()
-        stepsize //= 2
-        wibble /= wibbledecay
-
-    maparray -= maparray.min()
-    return maparray / maparray.max()
+        print(final.shape)
+        return torch.as_tensor(final)
 
 
 class SpeckleNoise(TUCorruption):
@@ -602,7 +637,7 @@ class GaussianBlur(TUCorruption):
         )
 
 
-class Saturate(ISaturation):
+class Saturation(ISaturation):
     def __init__(self, severity: int) -> None:
         super().__init__()
         if not (0 <= severity <= 5):
