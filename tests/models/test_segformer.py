8650ef3cb45871f108a56bd9074f58e1b17fe57f
Olivier <olivier.ar.laurent@gmail.com>
2024-11-19 11:37:42 +0100

    :shirt: Start improving eval loop code & simplify segformer tests & activate codecov test analysis

--- tests/models/test_segformer.py
+++ tests/models/test_segformer.py
@@ -12,8 +12,4 @@ class TestSegformer:
     def test_main(self):
         model = seg_former(10, 0)
         seg_former(10, 1)
-        seg_former(10, 2)
-        seg_former(10, 3)
-        seg_former(10, 4)
-        seg_former(10, 5)
         model(torch.randn(1, 3, 32, 32))
