45693a918eae479426569b8b7575111ad4e79f69
alafage <adrienlafage@outlook.com>
2024-12-09 17:41:53 +0100

    :white_check_mark: Add tests for convolutional distribution layers

--- tests/layers/test_distributions.py
+++ tests/layers/test_distributions.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 
 from torch_uncertainty.layers.distributions import (
+    get_dist_conv_layer,
     get_dist_linear_layer,
 )
 
@@ -11,8 +12,12 @@ def feat_input() -> torch.Tensor:
     return torch.rand((3, 8))  # (B, Hin)
 
 
-class TestNormalLinear:
-    """Testing the NormalLinear layer class."""
+def img_input() -> torch.Tensor:
+    return torch.rand((3, 2, 32, 32))  # (B, C, H, W)
+
+
+class TestDistributionLinear:
+    """Testing the distribution linear layer classes."""
 
     def test_normal_linear(self, feat_input: torch.Tensor):
         dist_layer = get_dist_linear_layer("normal")
@@ -89,3 +94,93 @@ class TestNormalLinear:
         assert out["lmbda"].shape == torch.Size([3, 2])
         assert out["alpha"].shape == torch.Size([3, 2])
         assert out["beta"].shape == torch.Size([3, 2])
+
+    def test_failures(self):
+        with pytest.raises(NotImplementedError):
+            get_dist_linear_layer("unknown")
+
+
+class TestDistributionConv:
+    """Testing the distribution convolutional layer classes."""
+
+    def test_normal_conv(self):
+        dist_layer = get_dist_conv_layer("normal")
+        layer = dist_layer(
+            base_layer=torch.nn.Conv2d,
+            event_dim=2,
+            in_channels=2,
+            kernel_size=3,
+        )
+        out = layer(img_input())
+        assert out.keys() == {"loc", "scale"}
+        assert out["loc"].shape == torch.Size([3, 2, 30, 30])
+        assert out["scale"].shape == torch.Size([3, 2, 30, 30])
+
+    def test_laplace_conv(self):
+        dist_layer = get_dist_conv_layer("laplace")
+        layer = dist_layer(
+            base_layer=torch.nn.Conv2d,
+            event_dim=2,
+            in_channels=2,
+            kernel_size=3,
+        )
+        out = layer(img_input())
+        assert out.keys() == {"loc", "scale"}
+        assert out["loc"].shape == torch.Size([3, 2, 30, 30])
+        assert out["scale"].shape == torch.Size([3, 2, 30, 30])
+
+    def test_cauchy_conv(self):
+        dist_layer = get_dist_conv_layer("cauchy")
+        layer = dist_layer(
+            base_layer=torch.nn.Conv2d,
+            event_dim=2,
+            in_channels=2,
+            kernel_size=3,
+        )
+        out = layer(img_input())
+        assert out.keys() == {"loc", "scale"}
+        assert out["loc"].shape == torch.Size([3, 2, 30, 30])
+        assert out["scale"].shape == torch.Size([3, 2, 30, 30])
+
+    def test_student_conv(self):
+        dist_layer = get_dist_conv_layer("student")
+        layer = dist_layer(
+            base_layer=torch.nn.Conv2d,
+            event_dim=2,
+            in_channels=2,
+            kernel_size=3,
+        )
+        out = layer(img_input())
+        assert out.keys() == {"loc", "scale", "df"}
+        assert out["loc"].shape == torch.Size([3, 2, 30, 30])
+        assert out["scale"].shape == torch.Size([3, 2, 30, 30])
+        assert out["df"].shape == torch.Size([3, 2, 30, 30])
+
+        layer = dist_layer(
+            base_layer=torch.nn.Conv2d,
+            event_dim=2,
+            in_channels=2,
+            kernel_size=3,
+            fixed_df=3.0,
+        )
+        out = layer(img_input())
+        assert out.keys() == {"loc", "scale", "df"}
+        assert out["loc"].shape == torch.Size([3, 2, 30, 30])
+        assert out["scale"].shape == torch.Size([3, 2, 30, 30])
+        assert out["df"].shape == torch.Size([3, 2, 30, 30])
+        assert torch.allclose(out["df"], torch.tensor(3.0))
+
+    def test_nig_conv(self):
+        dist_layer = get_dist_conv_layer("nig")
+        layer = dist_layer(
+            base_layer=torch.nn.Conv2d,
+            event_dim=2,
+            in_channels=2,
+            kernel_size=3,
+        )
+        out = layer(img_input())
+        assert out.keys() == {"loc", "lmbda", "alpha", "beta"}
+        assert out["loc"].shape == torch.Size([3, 2, 30, 30])
+        assert out["lmbda"].shape == torch.Size([3, 2, 30, 30])
+        assert out["alpha"].shape == torch.Size([3, 2, 30, 30])
+        assert out["beta"].shape == torch.Size([3, 2, 30, 30])
