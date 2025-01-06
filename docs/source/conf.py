c16d53eddec254ad05e0b3d24523b460d27c5802
alafage <adrienlafage@outlook.com>
2025-01-06 15:49:04 +0100

    :wrench: Increase package version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ copyright = (  # noqa: A001
     f"{datetime.now().year!s}, Adrien Lafage and Olivier Laurent"
 )
 author = "Adrien Lafage and Olivier Laurent"
-release = "0.3.1"
+release = "0.4.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
