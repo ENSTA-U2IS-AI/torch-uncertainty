a909027b937bbfee7db5ec3292cccfac7c8d5258
Anton Zamyatin <e12223389@student.tuwien.ac.at>
2025-03-01 22:43:00 +0100

    :bug: Seperate layer_args dicts for linear and conv2d layers in lenet.py

--- torch_uncertainty/models/lenet.py
+++ torch_uncertainty/models/lenet.py
@@ -20,7 +20,8 @@ class _LeNet(nn.Module):
         num_classes: int,
         linear_layer: type[nn.Module],
         conv2d_layer: type[nn.Module],
-        layer_args: dict,
+        linear_layer_args: dict,
+        conv2d_layer_args: dict,
         activation: Callable,
         norm: type[nn.Module],
         groups: int,
@@ -40,19 +41,19 @@ class _LeNet(nn.Module):
 
         self.dropout_rate = dropout_rate
 
-        self.conv1 = conv2d_layer(in_channels, 6, (5, 5), groups=groups, **layer_args)
+        self.conv1 = conv2d_layer(in_channels, 6, (5, 5), groups=groups, **conv2d_layer_args)
         if batchnorm:
             self.norm1 = norm(6)
         self.conv_dropout = nn.Dropout2d(p=dropout_rate)
-        self.conv2 = conv2d_layer(6, 16, (5, 5), groups=groups, **layer_args)
+        self.conv2 = conv2d_layer(6, 16, (5, 5), groups=groups, **conv2d_layer_args)
         if batchnorm:
             self.norm2 = norm(16)
         self.pooling = nn.AdaptiveAvgPool2d((4, 4))
-        self.fc1 = linear_layer(256, 120, **layer_args)
+        self.fc1 = linear_layer(256, 120, **linear_layer_args)
         self.fc_dropout = nn.Dropout(p=dropout_rate)
-        self.fc2 = linear_layer(120, 84, **layer_args)
+        self.fc2 = linear_layer(120, 84, **linear_layer_args)
         self.last_fc_dropout = nn.Dropout(p=dropout_rate)
-        self.fc3 = linear_layer(84, num_classes, **layer_args)
+        self.fc3 = linear_layer(84, num_classes, **linear_layer_args)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.conv_dropout(self.activation(self.norm1(self.conv1(x))))
@@ -72,7 +73,8 @@ def _lenet(
     stochastic: bool,
     in_channels: int,
     num_classes: int,
-    layer_args: dict,
+    linear_layer_args: dict,
+    conv2d_layer_args: dict,
     num_samples: int = 16,
     linear_layer: type[nn.Module] = nn.Linear,
     conv2d_layer: type[nn.Module] = nn.Conv2d,
@@ -89,7 +91,8 @@ def _lenet(
         activation=activation,
         norm=norm,
         groups=groups,
-        layer_args=layer_args,
+        linear_layer_args=linear_layer_args,
+        conv2d_layer_args=conv2d_layer_args,
         dropout_rate=dropout_rate,
     )
     if stochastic:
@@ -111,7 +114,8 @@ def lenet(
         num_classes=num_classes,
         linear_layer=nn.Linear,
         conv2d_layer=nn.Conv2d,
-        layer_args={},
+        linear_layer_args={},
+        conv2d_layer_args={},
         activation=activation,
         norm=norm,
         groups=groups,
@@ -137,7 +141,12 @@ def packed_lenet(
         linear_layer=PackedLinear,
         conv2d_layer=PackedConv2d,
         norm=norm,
-        layer_args={
+        linear_layer_args={
+            "num_estimators": num_estimators,
+            "alpha": alpha,
+            "gamma": gamma,
+        },
+        conv2d_layer_args={
             "num_estimators": num_estimators,
             "alpha": alpha,
             "gamma": gamma,
@@ -182,7 +191,8 @@ def bayesian_lenet(
         linear_layer=BayesLinear,
         conv2d_layer=BayesConv2d,
         norm=norm,
-        layer_args=layers_args,
+        linear_layer_args=layers_args,
+        conv2d_layer_args=layers_args,
         activation=activation,
         groups=groups,
         dropout_rate=dropout_rate,
