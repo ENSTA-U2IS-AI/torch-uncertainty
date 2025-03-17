e93a837fd612bc6dc0d70cddcf5d0aa094c46cba
Olivier <olivier.ar.laurent@gmail.com>
2025-03-17 19:05:53 +0100

    :zap: Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ copyright = (  # noqa: A001
     f"{datetime.now().year!s}, Adrien Lafage and Olivier Laurent"
 )
 author = "Adrien Lafage and Olivier Laurent"
-release = "0.4.2"
+release = "0.4.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
