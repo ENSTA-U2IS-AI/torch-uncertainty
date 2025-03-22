e232bd7ffd7931d5f9b62d1e07f35878b7a3795f
Olivier <olivier.ar.laurent@gmail.com>
2025-03-22 13:12:44 +0100

    :shirt: Format MUAD ttutorials

--- auto_tutorials_source/Ensemble_Methods/tutorial_muad_deep_en.py
+++ auto_tutorials_source/Ensemble_Methods/tutorial_muad_deep_en.py
@@ -12,27 +12,26 @@ For details and access, visit the `MUAD Website <https://muad-dataset.github.io/
 
 """
 
-# %% 
+# %%
 # 1. Load Muad dataset using Torch Uncertainty
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-# %% 
+# %%
 # Let's start by defining the training parameters.
 
-batch_size = 10
-learning_rate =1e-3
-weight_decay=2e-4
-lr_decay_epochs=20
-lr_decay=0.1
-nb_epochs=50
+BATCH_SIZE = 12
+MAX_EPOCHS = 50
+learning_rate = 1e-3
+weight_decay = 2e-4
+lr_decay_epochs = 20
+lr_decay = 0.1
 
-# %% 
-# In this Tutorial we are using the small version a bigger version can be specified with keyword "full" instead of small.
-
+# %%
+# In this Tutorial we are using the small version of the dataset.
+# A larger version can be specified with keyword "full" instead of small.
 
 import torch
-from einops import rearrange
 from torchvision import tv_tensors
 from torchvision.transforms import v2
 from torchvision.transforms.v2 import functional as F
@@ -70,11 +69,32 @@ val_transform = v2.Compose(
     ]
 )
 
-train_set = MUAD(root="./data", target_type="semantic", version="small", split="train" , transforms=train_transform, download=True)
-val_set = MUAD(root="./data", target_type="semantic", version="small", split="val" , transforms=val_transform, download=True)
-test_set = MUAD(root="./data", target_type="semantic", version="small", split="test" , transforms=val_transform, download=True)
+train_set = MUAD(
+    root="./data",
+    target_type="semantic",
+    version="small",
+    split="train",
+    transforms=train_transform,
+    download=True,
+)
+val_set = MUAD(
+    root="./data",
+    target_type="semantic",
+    version="small",
+    split="val",
+    transforms=val_transform,
+    download=True,
+)
+test_set = MUAD(
+    root="./data",
+    target_type="semantic",
+    version="small",
+    split="test",
+    transforms=val_transform,
+    download=True,
+)
 
-# %% 
+# %%
 # Visualize a validation input sample (and RGB image)
 
 # Undo normalization on the image and convert to uint8.
@@ -86,12 +106,12 @@ img = img * std[:, None, None] + mean[:, None, None]
 img = F.to_dtype(img, torch.uint8, scale=True)
 img_pil = F.to_pil_image(img)
 
-plt.figure(figsize=(6,6))
+plt.figure(figsize=(6, 6))
 plt.imshow(img_pil)
-plt.axis("off") 
+plt.axis("off")
 plt.show()
 
-# %% 
+# %%
 # Visualize the same image above but segmented.
 
 from torchvision.utils import draw_segmentation_masks
@@ -101,14 +121,14 @@ tgt_masks = tmp_tgt == torch.arange(22, device=tgt.device)[:, None, None]
 img_segmented = draw_segmentation_masks(img, tgt_masks, alpha=1, colors=val_set.color_palette)
 img_pil = F.to_pil_image(img)
 
-plt.figure(figsize=(6,6))
+plt.figure(figsize=(6, 6))
 plt.imshow(img_pil)
-plt.axis("off") 
+plt.axis("off")
 plt.show()
 
-# %% 
+# %%
 # Below is the complete list of classes in MUAD, presented as:
-# 
+#
 # 1.   Class Name
 # 2.   Train ID
 # 3.   Segmentation Color in RGB format [R,G, B].
@@ -119,30 +139,18 @@ for muad_class in train_set.classes:
     color = muad_class.color
     print(f"Class: {class_name}, Train ID: {train_id}, Color: {color}")
 
-# %% 
+# %%
 # Let's now calculate each class weight
 
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
-train_loader = DataLoader(
-        train_set,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=4)
+train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
-val_loader = DataLoader(
-        val_set,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=4)
+val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
-test_loader = DataLoader(
-        test_set,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=4)
+test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
 
 def enet_weighing(dataloader, num_classes, c=1.02):
@@ -169,19 +177,20 @@ def enet_weighing(dataloader, num_classes, c=1.02):
     class_count = 0
     total = 0
     for _, label in dataloader:
-      label = label.cpu().numpy()
-      # Flatten label
-      flat_label = label.flatten()
-      flat_label = flat_label[flat_label != 255]
+        label = label.cpu().numpy()
+        # Flatten label
+        flat_label = label.flatten()
+        flat_label = flat_label[flat_label != 255]
 
-      # Sum up the number of pixels of each class and the total pixel
-      # counts for each label
-      class_count += np.bincount(flat_label, minlength=num_classes)
-      total += flat_label.size
+        # Sum up the number of pixels of each class and the total pixel
+        # counts for each label
+        class_count += np.bincount(flat_label, minlength=num_classes)
+        total += flat_label.size
 
     # Compute propensity score and then the weights for each class
     propensity_score = class_count / total
     return 1 / (np.log(c + propensity_score))
+
 
 print("\nComputing class weights...")
 print("(this can take a while depending on the dataset size)")
@@ -189,7 +198,7 @@ class_weights = enet_weighing(train_loader, 19)
 class_weights = torch.from_numpy(class_weights).float()
 print("Class weights:", class_weights)
 
-# %% 
+# %%
 # 2. Building the DNN
 # ~~~~~~~~~~~~~~~~~~~
 
@@ -208,7 +217,7 @@ class DoubleConv(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
         )
 
     def forward(self, x):
@@ -227,10 +236,7 @@ class InConv(nn.Module):
 class Down(nn.Module):
     def __init__(self, in_ch, out_ch):
         super().__init__()
-        self.mpconv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_ch, out_ch)
-        )
+        self.mpconv = nn.Sequential(nn.MaxPool2d(2), DoubleConv(in_ch, out_ch))
 
     def forward(self, x):
         return self.mpconv(x)
@@ -247,8 +253,11 @@ class Up(nn.Module):
 
     def forward(self, x1, x2):
         if self.bilinear:
-            x1 = F.resize(x1, size=[2*x1.size()[2],2*x1.size()[3]],
-                          interpolation=v2.InterpolationMode.BILINEAR)
+            x1 = F.resize(
+                x1,
+                size=[2 * x1.size()[2], 2 * x1.size()[3]],
+                interpolation=v2.InterpolationMode.BILINEAR,
+            )
         else:
             x1 = self.up(x1)
 
@@ -256,8 +265,7 @@ class Up(nn.Module):
         diff_y = x2.size()[2] - x1.size()[2]
         diff_x = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, [diff_x // 2, diff_x - diff_x // 2,
-                        diff_y // 2, diff_y - diff_y // 2])
+        x1 = F.pad(x1, [diff_x // 2, diff_x - diff_x // 2, diff_y // 2, diff_y - diff_y // 2])
 
         # for padding issues, see
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
@@ -275,7 +283,9 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-#please note that we have added dropout layer to be abble to use MC dropout
+
+# please note that we have added dropout layer to be abble to use MC dropout
+
 
 class UNet(nn.Module):
     def __init__(self, classes):
@@ -309,11 +319,11 @@ class UNet(nn.Module):
         return self.outc(x)
 
 
-# %% 
+# %%
 # 3. Training
 # ~~~~~~~~~~~
 # In this part we will train a `Deep Ensembles <https://papers.nips.cc/paper/2017/file/9ef2ed4b7fd2c810847ffa5fa85bce38-Paper.pdf>`_ easily using TorchUncertainty.
-# 
+#
 
 # %%
 # Define the model
@@ -330,7 +340,7 @@ ensemble = deep_ensembles(
 )
 
 
-# %% 
+# %%
 # Train on 1 epoch for demonstration purposes
 
 from torch_uncertainty import TUTrainer
@@ -340,15 +350,11 @@ from torch.optim import lr_scheduler
 
 # We build the optimizer
 optimizer = optim.Adam(
-	ensemble.parameters(),
-	lr=learning_rate*num_estimators,
-	weight_decay=weight_decay
+    ensemble.parameters(), lr=learning_rate * num_estimators, weight_decay=weight_decay
 )
 
 # Learning rate decay scheduler
-lr_updater = lr_scheduler.StepLR(
-    optimizer, lr_decay_epochs, lr_decay
-)
+lr_updater = lr_scheduler.StepLR(optimizer, lr_decay_epochs, lr_decay)
 
 # Initialize the trainer
 trainer = TUTrainer(accelerator="gpu", devices=1, max_epochs=1, precision=16, logger=False)
@@ -361,20 +367,22 @@ ens_routine = SegmentationRoutine(
 )
 
 # %%
-#trainer.fit(ens_routine, train_loader, val_loader)
+# trainer.fit(ens_routine, train_loader, val_loader)
 
-# %% 
+# %%
 # Test your ensemble obtained.
 
-#results = trainer.test(ens_routine, test_loader)
+# results = trainer.test(ens_routine, test_loader)
 
-# %% 
+# %%
 # Load a pre-trained ensembles from huggingface to continue the tutorial
 
 from huggingface_hub import hf_hub_download
 
 # Download the model
-model_path = hf_hub_download(repo_id="torch-uncertainty/muad_tutorials", filename="ensemble_tuto.pth")
+model_path = hf_hub_download(
+    repo_id="torch-uncertainty/muad_tutorials", filename="ensemble_tuto.pth"
+)
 
 model = deep_ensembles(
     UNet(classes=19),
@@ -382,16 +390,17 @@ model = deep_ensembles(
     task="segmentation",
 )
 model.load_state_dict(torch.load(model_path))
-model = model.to('cpu')
+model = model.to("cpu")
 
-# %% 
+# %%
 # 4. Uncertainty evaluations with MCP
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Here we will just use as confidence score the Maximum class probability (MCP)
-# 
+#
 
 # %%
 import matplotlib.pyplot as plt
+
 sample_idx = 0
 img, target = test_set[sample_idx]
 
@@ -399,14 +408,14 @@ batch_img = img.unsqueeze(0)
 batch_target = target.unsqueeze(0)
 model.eval()
 with torch.no_grad():
-	# Forward propagation
-	outputs = model(batch_img)
-	outputs_proba = outputs.softmax(dim=1)
-	outputs_proba = outputs_proba.mean(dim=0)
+    # Forward propagation
+    outputs = model(batch_img)
+    outputs_proba = outputs.softmax(dim=1)
+    outputs_proba = outputs_proba.mean(dim=0)
 
-	# remove the batch dimension
-	outputs_proba = outputs_proba.squeeze(0)
-	confidence, pred = outputs_proba.max(0)
+    # remove the batch dimension
+    outputs_proba = outputs_proba.squeeze(0)
+    confidence, pred = outputs_proba.max(0)
 
 # Undo normalization on the image and convert to uint8.
 mean = torch.tensor([0.485, 0.456, 0.406], device=img.device)
@@ -437,11 +446,18 @@ ax3.imshow(pred_img)
 ax4.imshow(confidence_img)
 plt.show()
 
-# %% 
+# %%
 # Now let's load the OOD test set
 
 # %%
-test_ood_set = MUAD(root="./data", target_type="semantic", version="small", split="ood" , transforms=val_transform, download=True)
+test_ood_set = MUAD(
+    root="./data",
+    target_type="semantic",
+    version="small",
+    split="ood",
+    transforms=val_transform,
+    download=True,
+)
 
 # %%
 # Testing on the ood set
@@ -453,14 +469,14 @@ batch_img = img.unsqueeze(0)
 batch_target = target.unsqueeze(0)
 model.eval()
 with torch.no_grad():
-	# Forward propagation
-	outputs = model(batch_img)
-	outputs_proba = outputs.softmax(dim=1)
-	outputs_proba = outputs_proba.mean(dim=0)
-	
-	# remove the batch dimension
-	outputs_proba = outputs_proba.squeeze(0)
-	confidence, pred = outputs_proba.max(0)
+    # Forward propagation
+    outputs = model(batch_img)
+    outputs_proba = outputs.softmax(dim=1)
+    outputs_proba = outputs_proba.mean(dim=0)
+
+    # remove the batch dimension
+    outputs_proba = outputs_proba.squeeze(0)
+    confidence, pred = outputs_proba.max(0)
 
 # Undo normalization on the image and convert to uint8.
 mean = torch.tensor([0.485, 0.456, 0.406], device=img.device)
