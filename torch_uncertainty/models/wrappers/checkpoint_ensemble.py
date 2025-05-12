62d3dd1ececc17dba55a1937b028ec9902116160
Olivier <olivier.ar.laurent@gmail.com>
2025-05-12 17:26:17 +0200

    :sparkles: Add cycles to CheckpointEnsembles

--- torch_uncertainty/models/wrappers/checkpoint_ensemble.py
+++ torch_uncertainty/models/wrappers/checkpoint_ensemble.py
@@ -8,6 +8,8 @@ class CheckpointEnsemble(nn.Module):
     def __init__(
         self,
         model: nn.Module,
+        cycle_start: int | None = None,
+        cycle_length: int | None = None,
         save_schedule: list[int] | None = None,
         use_final_model: bool = True,
         store_on_cpu: bool = False,
@@ -16,50 +18,86 @@ class CheckpointEnsemble(nn.Module):
 
         Args:
             model (nn.Module): The model to train and ensemble.
-            save_schedule (list[int] | None): The epochs at which to save the model. If save schedule is
-                ``None``, save the model at every epoch. Defaults to ``None``.
+            cycle_start (int): Epoch to start ensembling. Defaults to ``None``.
+            cycle_length (int): Number of epochs between model collections. Defaults to ``None``.
+            save_schedule (list[int] | None): The epochs at which to save the model. Defaults to ``None``.
             use_final_model (bool): Whether to use the final model as a checkpoint. Defaults to ``True``.
             store_on_cpu (bool): Whether to put the models on the CPU when unused. Defaults to ``False``.
+
+        Note:
+            The models are saved at the end of the specified epochs.
+
+        Note:
+            If :attr:`cycle_start`, :attr:`cycle_length` and :attr:`save_schedule` are ``None``,
+            the wrapper will save the models at each epoch.
 
         Reference:
             Checkpoint Ensembles: Ensemble Methods from a Single Training Process.
             Hugh Chen, Scott Lundberg, Su-In Lee. In ArXiv 2018.
         """
         super().__init__()
+        self.mode = None
+        if (cycle_start is None) + (cycle_length is None) + (save_schedule is None) == 0:
+            self.mode = "all"
+        elif (cycle_start is not None) and (cycle_length is not None) and save_schedule is None:
+            self.mode = "cycle"
+        elif save_schedule is not None and (cycle_start is None) and (cycle_length is None):
+            self.mode = "schedule"
+        else:
+            raise ValueError(
+                f"The combination of arguments: cycle_start: {cycle_start}, cycle_length: {cycle_length}, save_schedule: {save_schedule} is not known."
+            )
+
         self.core_model = model
+        self.cycle_start = cycle_start
+        self.cycle_length = cycle_length
         self.save_schedule = save_schedule
+
         self.use_final_model = use_final_model
         self.store_on_cpu = store_on_cpu
-        self.num_estimators = int(use_final_model)
+        self.register_buffer("num_estimators", torch.tensor(use_final_model, dtype=torch.long))
         self.saved_models = nn.ModuleList()
+
+    def _load_from_state_dict(
+        self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs
+    ):
+        for _ in range(state_dict["model.num_estimators"] - 1):
+            self.saved_models.append(copy.deepcopy(self.core_model))
+        return super()._load_from_state_dict(
+            state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs
+        )
 
     @torch.no_grad()
     def update_wrapper(self, epoch: int) -> None:
-        """Save the model at the given epoch if included in the schedule.
+        """Save the model at the end of the epoch, if included in the schedule.
 
         Args:
             epoch (int): The current epoch.
         """
-        if self.save_schedule is None or epoch in self.save_schedule:
-            self.saved_models.append(
-                copy.deepcopy(self.core_model)
-                if not self.store_on_cpu
-                else copy.deepcopy(self.core_model).cpu()
-            )
-            self.num_estimators += 1
+        match self.mode:
+            case "schedule":
+                if epoch not in self.save_schedule:
+                    return
+            case "cycle":
+                if epoch < self.cycle_start or (epoch - self.cycle_start) % self.cycle_length != 0:
+                    return
+        self.saved_models.append(
+            copy.deepcopy(self.core_model)
+            if not self.store_on_cpu
+            else copy.deepcopy(self.core_model).cpu()
+        )
+        self.num_estimators += 1
 
     def eval_forward(self, x: Tensor) -> Tensor:
         """Forward pass for evaluation.
 
-        If the model is in evaluation mode, this method will return the
-        ensemble prediction. Otherwise, it will return the prediction of the
-        current model.
+        This method will return the ensemble prediction if models have already been collected.
 
         Args:
             x (Tensor): The input tensor.
 
         Returns:
-            Tensor: The model or ensemble output.
+            Tensor: The ensemble output.
         """
         preds: list[Tensor] = []
         if not len(self.saved_models):
@@ -76,11 +114,27 @@ class CheckpointEnsemble(nn.Module):
         return torch.cat(preds, dim=0)
 
     def forward(self, x: Tensor) -> Tensor:
+        """Forward pass for training and evaluation mode.
+
+        If the model is in evaluation mode, this method will return the
+        ensemble prediction. Otherwise, it will return the prediction of the
+        current model.
+
+        Args:
+            x (Tensor): The input tensor.
+
+        Returns:
+            Tensor: The model or ensemble output.
+        """
         if self.training:
             return self.core_model.forward(x)
         return self.eval_forward(x)
 
     def to(self, *args, **kwargs):
+        """Move the model and change its type.
+
+        If :attr:`store_on_cpu` is set to True, we force device to "cpu" to avoid filling the VRAM.
+        """
         device, dtype, non_blocking = torch._C._nn._parse_to(*args, **kwargs)[:3]
 
         if self.store_on_cpu:
