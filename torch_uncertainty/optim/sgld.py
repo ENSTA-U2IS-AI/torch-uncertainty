1c5cd67892148a77b8ee49d89f4c2f3f30118a10
Olivier <olivier.ar.laurent@gmail.com>
2025-05-13 16:08:10 +0200

    :hammer: Rework SGLD

--- torch_uncertainty/optim/sgld.py
+++ torch_uncertainty/optim/sgld.py
@@ -8,26 +8,25 @@ class SGLD(Optimizer):
     def __init__(
         self,
         params: ParamsT,
-        dataset_size: int,
-        lr: float = 1e-5,
+        lr: float = 1e-3,
+        noise_factor: float = 1e-2,
         weight_decay: float = 0,
     ) -> None:
         r"""Stochastic Gradient Langevin Dynamics (SGLD) optimizer.
-
-        TODO: dynamical equations
 
         Args:
             params (ParamsT): Iterable of parameters or named_parameters to optimize or iterable of
                 dicts defining parameter groups. When using named_parameters, all parameters in all
                 groups should be named.
-            dataset_size (int): Size of the dataset. Used to scale the gradient.
-            lr (float): Learning rate. Defaults to ``1e-3``.
+            lr (float): Learning rate for the optimization. Defaults to ``1e-3``.
+            noise_factor (float): A factor to reduce the amount of noise and stabilize the training.
+                This parameter was not proposed in the original paper. Defaults to ``1e-2``.
             weight_decay (float, optional): Weight decay (L2 penalty). Defaults to ``0``.
 
         Reference:
             - `Bayesian Learning via Stochastic Gradient Langevin Dynamics <https://www.stats.ox.ac.uk/~teh/research/compstats/WelTeh2011a.pdf>`_.
         """
-        defaults = {"dataset_size": dataset_size, "lr": lr, "weight_decay": weight_decay}
+        defaults = {"lr": lr, "noise_factor": noise_factor, "weight_decay": weight_decay}
         super().__init__(params, defaults)
 
     def step(self, closure: Callable[[], float] | None = None) -> float:
@@ -36,19 +35,25 @@ class SGLD(Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-            dataset_size = group["dataset_size"]
             lr = group["lr"]
+            noise_factor = group["noise_factor"]
             weight_decay = group["weight_decay"]
 
             for p in group["params"]:
                 if p.grad is None:
                     continue
+
+                # original gradient
                 d_p = p.grad.data
 
+                # Centered Gaussian assumption
                 if weight_decay != 0:
-                    d_p.add_(p.data, alpha=weight_decay / dataset_size)
+                    d_p.add_(p.data, alpha=weight_decay)
 
-                noise = torch.randn_like(p.data)
-                p.data.add_(-lr * dataset_size * 0.5 * d_p - ((lr**0.5) * noise))
+                # Noise of variance 2*lr
+                noise = torch.randn_like(p) * ((2 * lr) ** 0.5) * noise_factor
+
+                # Weight update
+                p.data.add_(-lr * d_p + noise)
 
         return loss
