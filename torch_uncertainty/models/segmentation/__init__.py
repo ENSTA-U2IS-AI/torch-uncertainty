ecbe015ca771ec17f800e027158b3f8aba545827
alafage <adrienlafage@outlook.com>
2025-05-15 18:57:16 +0200

    :construction:

--- torch_uncertainty/models/segmentation/__init__.py
+++ torch_uncertainty/models/segmentation/__init__.py
@@ -3,6 +3,8 @@ from .deeplab import deep_lab_v3_resnet50, deep_lab_v3_resnet101
 from .unet import (
     batched_small_unet,
     batched_unet,
+    masked_small_unet,
+    masked_unet,
     mimo_small_unet,
     mimo_unet,
     packed_small_unet,
