ef69039ff65321983e7c906cca52a4b8103e8a91
Olivier <olivier.ar.laurent@gmail.com>
2025-05-19 20:46:29 +0200

    :bug: Fix Zero

--- torch_uncertainty/models/wrappers/zero.py
+++ torch_uncertainty/models/wrappers/zero.py
@@ -5,7 +5,9 @@ from torch.special import entr
 
 
 class Zero(nn.Module):
-    def __init__(self, model: nn.Module, num_tta: int, filter_views: float = 0.1) -> None:
+    def __init__(
+        self, model: nn.Module, num_tta: int, filter_views: float = 0.1, eps: float = 1e-8
+    ) -> None:
         """Zero for test-time adaptation.
 
         Zero performs "0-temperature averaging" (i.e. majority voting) at evaluation. It starts
@@ -17,19 +19,22 @@ class Zero(nn.Module):
             model (nn.Module): The inner model to train.
             num_tta (int): The number of views at evaluation time.
             filter_views (float): Filter out 1-:attr:`filter_views` of the predictions of the augmented views.
+                Defaults to ``0.1``.
+            eps (float): for computational stability. Defaults to ``1e-8``;
         """
         super().__init__()
-        _zero_checks(num_tta, filter_views)
+        _zero_checks(num_tta, filter_views, eps)
         self.core_model = model
         self.filter = filter_views
         self.kept_views = int(filter_views * num_tta)
         self.num_tta = num_tta
+        self.eps = eps
 
     def eval_forward(self, x: Tensor) -> Tensor:
         # predict and separate the views from the batch
         all_predictions = rearrange(self.core_model(x), "(b v) c -> b v c", v=self.num_tta)
         batch_size, _, num_classes = all_predictions.shape
-        entropies = entr(all_predictions).sum(1)
+        entropies = entr(all_predictions).sum(2)
 
         # Get the index of the most confident predictions on the views
         conf_idx = torch.argsort(entropies, dim=-1)
@@ -43,15 +48,18 @@ class Zero(nn.Module):
             )
             maximum = predictions[img_id, :].max()
             i = 0
-            # If the maximum corresponds to two predictions, look at an additional
-            while i < self.num_tta and torch.sum(1 * (predictions[img_id, :] == maximum)) > 1:
+            # If the maximum is shared among two predictions, look at an additional one
+            while (
+                self.kept_views + i < self.num_tta
+                and torch.sum(1 * (predictions[img_id, :] == maximum)) > 1
+            ):
                 predictions[img_id, img_votes[conf_idx[img_id, self.kept_views + i]]] += 1
                 maximum = predictions[img_id, :].max()
                 i += 1
 
         predictions /= self.num_tta
         # We will apply the softmax in the routine, so let's apply its inverse here
-        return predictions.log()
+        return (predictions + self.eps).log()
 
     def forward(self, x: Tensor) -> Tensor:
         if self.training:
@@ -59,10 +67,12 @@ class Zero(nn.Module):
         return self.eval_forward(x)
 
 
-def _zero_checks(num_tta: int, filter_views: float) -> None:
-    if filter_views <= 0.0 or filter_views >= 1.0:
-        raise ValueError(f"`filter_views` must be in the range (0, 1). Got {filter_views}.")
+def _zero_checks(num_tta: int, filter_views: float, eps: float) -> None:
+    if filter_views <= 0.0 or filter_views > 1.0:
+        raise ValueError(f"`filter_views` must be in the range ]0, 1]. Got {filter_views}.")
     if num_tta < 1 / filter_views:
         raise ValueError(
             f"`num_tta` should be greater than 1/filter_views to use Zero. Got {num_tta} < {1 / filter_views}."
         )
+    if eps <= 0:
+        raise ValueError(f"`eps` should be strictly positive. Got {eps}.")
