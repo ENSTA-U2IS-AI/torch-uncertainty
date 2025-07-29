5db4fb67a78c18ed1d37e6229e5179c29d672611
Olivier Laurent <olivier.ar.laurent@gmail.com>
2025-07-29 23:15:21 +0200

    :bug: Rebase

--- auto_tutorial_source/Classification/tutorial_distribution_shift.py
+++ auto_tutorial_source/Classification/tutorial_distribution_shift.py
@@ -1,4 +1,4 @@
-# ruff: noqa: E402, E703, D212, D415, T201
+# ruff: noqa: D212, D415, T201
 """
 Evaluating Model Performance Under Distribution Shift with TorchUncertainty
 ===========================================================================
