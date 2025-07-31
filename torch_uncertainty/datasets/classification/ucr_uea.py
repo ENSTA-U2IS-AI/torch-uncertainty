94af1d5eb80c5e4ced57247172cd7bbb04d26a0d
Olivier Laurent <olivier.ar.laurent@gmail.com>
2025-07-31 11:52:54 +0200

    :zap: Update version number

--- torch_uncertainty/datasets/classification/ucr_uea.py
+++ torch_uncertainty/datasets/classification/ucr_uea.py
@@ -41,8 +41,8 @@ class UCRUEADataset(Dataset):
         """
         if not tslearn_installed:  # coverage: ignore
             raise ImportError(
-                "The tslearn library is not installed. Please install"
-                "torch_uncertainty with the timeseries option:"
+                "The tslearn library is not installed. Please install "
+                "torch_uncertainty with the timeseries option: "
                 """pip install -U "torch_uncertainty[timeseries]"."""
             )
 
