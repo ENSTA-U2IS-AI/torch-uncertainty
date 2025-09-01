b3e883fe639185f80418d0be8d60a529a76fddb4
alafage <adrienlafage@outlook.com>
2025-09-01 17:31:13 +0200

    :art: Fix outdated config files for all experiments

---
+++ experiments/depth/kitti/main.py
@@ -0,0 +1,20 @@
+import torch
+
+from torch_uncertainty import TULightningCLI
+from torch_uncertainty.datamodules.depth import KITTIDataModule
+from torch_uncertainty.routines import PixelRegressionRoutine
+
+
+def cli_main() -> TULightningCLI:
+    return TULightningCLI(PixelRegressionRoutine, KITTIDataModule)
+
+
+if __name__ == "__main__":
+    torch.set_float32_matmul_precision("medium")
+    cli = cli_main()
+    if (
+        (not cli.trainer.fast_dev_run)
+        and cli.subcommand == "fit"
+        and cli._get(cli.config, "eval_after_fit")
+    ):
+        cli.trainer.test(datamodule=cli.datamodule, ckpt_path="best")
