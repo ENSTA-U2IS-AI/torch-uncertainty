528883868383ee9f3c87bb789386d75a564707be
Olivier Laurent <olivier.ar.laurent@gmail.com>
2026-01-12 10:19:42 +0100

    :zap: Bump to version 0.8 & update packages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ copyright = (  # noqa: A001
     f"{datetime.now().year!s}, Adrien Lafage and Olivier Laurent"
 )
 author = "Adrien Lafage and Olivier Laurent"
-release = "0.7.0.post1"
+release = "0.8.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
